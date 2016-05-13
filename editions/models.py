52635310faae9c8ac7089128583c26c95abd1bc9
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2016-05-13 11:01:31 +0200

    added edit edition button

--- editions/models.py
+++ editions/models.py
@@ -41,6 +41,9 @@ class Period(models.Model):
 class Language(models.Model):
     iso_code = models.CharField(max_length=3, blank=True, null=True)
 
+    def __str__(self):
+        return "{}".format(self.iso_code)
+
 
 class Edition(models.Model):
     """Holds the (meta)data of a digital editions project. Information taken from

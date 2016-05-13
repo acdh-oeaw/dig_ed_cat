c6f50d3fcc9d31c50db073a34e1c83d8a70f4044
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2016-05-13 10:46:54 +0200

    * leaflet now loaded from static files * fixed place views.py

--- places/views.py
+++ places/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import (render, render_to_response, get_object_or_404,
 from django.views import generic
 from django.views.generic.edit import DeleteView
 from django.core.urlresolvers import reverse, reverse_lazy
-
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from .models import Place
 from .forms import PlaceForm
 
@@ -17,6 +18,7 @@ class PlaceListView(generic.ListView):
 		return Place.objects.all()
 
 
+@login_required
 def create_place(request):
 	if request.method == "POST":
 		form = PlaceForm(request.POST)
@@ -29,20 +31,26 @@ def create_place(request):
 		form = PlaceForm()
 		return render(request, 'places/edit_places.html', {'form':form})
 
-
+@login_required
 def edit_place(request, pk):
     instance = Place.objects.get(id=pk)
     username = "&username=digitalarchiv"
     if request.method == "GET":
         placeName = instance.name
         root = "http://api.geonames.org/searchJSON?q="
-        params = "&fuzzy=0.6&lang=de&maxRows=100"
+        params = "&fuzzy=0.6&lang=en&maxRows=100"
         url = root+placeName+params+username
         try:
             r = requests.get(url)
             response = r.text
             responseJSON = json.loads(response)
             responseJSON = responseJSON['geonames']
+            form = PlaceForm(instance = instance)
+            print(url)
+            return render(
+                request, 'places/edit_places.html',
+                {'object':instance, 'form':form, 'responseJSON':responseJSON}
+            )
         except requests.exceptions.RequestException as e:
             url = e
         form = PlaceForm(instance = instance)
@@ -60,6 +68,10 @@ def edit_place(request, pk):
 
 
 class PlaceDelete(DeleteView):
-	model = Place
-	template_name = 'webpage/confirm_delete.html'
-	success_url = reverse_lazy('places:place_list')
+    model = Place
+    template_name = 'webpage/confirm_delete.html'
+    success_url = reverse_lazy('places:place_list')
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(PlaceDelete, self).dispatch(*args, **kwargs)

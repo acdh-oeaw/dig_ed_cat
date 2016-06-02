212c56c0780545d81caa721fcdda814529222ba5
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2016-06-02 17:42:14 +0200

    added imprint https://clarin.arz.oeaw.ac.at/redmine/issues/6111

--- webpage/urls.py
+++ webpage/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-url(r'^$', views.start_view, name="start"),
-url(r'^login/$', views.user_login, name='user_login'),
-url(r'^accounts/login/$', views.user_login, name='user_login'),
-url(r'^logout/$', views.user_logout, name='user_logout'),
+    url(r'^$', views.start_view, name="start"),
+    url(r'^login/$', views.user_login, name='user_login'),
+    url(r'^accounts/login/$', views.user_login, name='user_login'),
+    url(r'^logout/$', views.user_logout, name='user_logout'),
+    url(r'^imprint/$', views.imprint, name='imprint'),
 ]
--- webpage/views.py
+++ webpage/views.py
@@ -1,44 +1,44 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
-from django.shortcuts import render, render_to_response, get_object_or_404, redirect
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from django.core.urlresolvers import reverse, reverse_lazy
 
 from .forms import form_user_login
 
 
+def imprint(request):
+    context = RequestContext(request)
+    return render(request, 'webpage/imprint.html', context)
+
+
 def start_view(request):
-	context = RequestContext(request)
-	return render(request, 'webpage/index.html', context)
+    context = RequestContext(request)
+    return render(request, 'webpage/index.html', context)
 
 
 #################################################################
-#				views for login/logout							#
+#               views for login/logout                          #
 #################################################################
 
 def user_login(request):
-	errors=[]
-	if request.method == 'POST':
-		form = form_user_login(request.POST)
-		if form.is_valid():
-			cd = form.cleaned_data
-			user = authenticate(username=cd['username'],password=cd['password'])
-			if user is not None:
-				if user.is_active:
-					login(request, user)
-					return HttpResponseRedirect(request.GET.get('next','/'))
-				else:
-					return HttpResponse('not active.')
-			else:
-				return HttpResponse('user does not exist')
-	else:
-		form = form_user_login()
-		return render(request, 'webpage/user_login.html', {'form':form})
+    if request.method == 'POST':
+        form = form_user_login(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect(request.GET.get('next', '/'))
+                else:
+                    return HttpResponse('not active.')
+            else:
+                return HttpResponse('user does not exist')
+    else:
+        form = form_user_login()
+        return render(request, 'webpage/user_login.html', {'form': form})
 
 
 def user_logout(request):
-	logout(request)
-	return render_to_response('webpage/user_logout.html')
+    logout(request)
+    return render_to_response('webpage/user_logout.html')

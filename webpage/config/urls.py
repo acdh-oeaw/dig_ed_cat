be3c4defb8562b091b4149a8585ca55fbbae23b2
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-02-17 14:37:41 +0100

    fixed https://github.com/acdh-oeaw/dig_ed_cat/issues/33 and https://github.com/acdh-oeaw/dig_ed_cat/issues/32

--- webpage/config/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-from autocomplete_light import shortcuts as al
-al.autodiscover()
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('webpage.urls', namespace='webpage')),

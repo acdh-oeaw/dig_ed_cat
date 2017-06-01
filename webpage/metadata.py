3a50f99b38a8a5fb008c250d0b79fd6e16466f45
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-06-01 09:38:10 +0200

    metadata config file added https://github.com/acdh-oeaw/dig_ed_cat/issues/86

---
+++ webpage/metadata.py
@@ -0,0 +1,13 @@
+# this files contains basic metadata about the project. This data will be used
+# (by default) in the base.html and index.html
+
+PROJECT_METADATA = {
+    'title': 'Dig-Ed-Cat',
+    'author': 'Peter Andorfer, Ksenia Zaytseva, Greta Franzini',
+    'subtitle': 'Catalogue of Digital Editions',
+    'description': 'A web application based Catalogue of Digital Editions',
+    'github': 'https://github.com/acdh-oeaw/dig-ed-cat',
+    'purpose_de': 'Diese Website dient der Information über das Projekt Digital Editions Catalogue und der Bereitstellung der aus diesem Projekt hervorgehenden Forschungsdaten und -ergebnisse.',
+    'purpose_en': 'This website provides information on the project Digital Editions Catalogue as well as the data and results emerging from this project.',
+    'verion': '2.0.1'
+}

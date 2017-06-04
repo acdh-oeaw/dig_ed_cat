c6cf25c6e82e9b1e321bfea58c05f9ce7d28e906
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-06-04 17:57:11 +0200

    removed news app

--- news/api_views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .serializers import NewsFeedSerializer
-from .models import NewsFeed
-
-
-class NewsFeedViewSet(viewsets.ModelViewSet):
-    queryset = NewsFeed.objects.all()
-    serializer_class = NewsFeedSerializer
--- news/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import NewsFeed
-
-
-class NewsFeedSerializer(serializers.HyperlinkedModelSerializer):
-
-    class Meta:
-        exclude = ['author']
-        model = NewsFeed

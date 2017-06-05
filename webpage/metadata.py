fa83c046cd9d178810d23a0a76afaa8a60b02c87
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-06-05 18:30:51 +0200

    changed version to 3.0.0 due to using news-package

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -9,5 +9,5 @@ PROJECT_METADATA = {
     'github': 'https://github.com/acdh-oeaw/dig-ed-cat',
     'purpose_de': 'Diese Website dient der Information über das Projekt Digital Editions Catalogue und der Bereitstellung der aus diesem Projekt hervorgehenden Forschungsdaten und -ergebnisse.',
     'purpose_en': 'This website provides information on the project Digital Editions Catalogue as well as the data and results emerging from this project.',
-    'version': '2.1.0'
+    'version': '3.0.0'
 }

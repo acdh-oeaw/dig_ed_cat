99449a935c7a54ffa1f55a75a309c903f3fa3019
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-06-28 11:58:13 +0200

    removed handle-app because this is now included as django-package, updated requirements

--- handle/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from . models import Pid
-
-admin.site.register(Pid)
--- handle/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HandleConfig(AppConfig):
-    name = 'handle'

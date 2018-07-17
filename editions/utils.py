db9d030a933be8a301607893174375ed004c5941
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-07-17 13:09:22 +0200

    updated sync script to fix #192

--- editions/utils.py
+++ editions/utils.py
@@ -1,7 +1,27 @@
 import pandas as pd
 
-from editions.models import Institution
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ObjectDoesNotExist
+from editions.models import Institution, Person, Period, Language, Edition
 from places.models import Place
+from handle.models import Pid
+
+
+BOOLEAN_CHOICES = {
+    "1": "yes",
+    "0": "no",
+    "no information provided": "no information provided",
+    "not provided": "no information provided",
+    "False": "no information provided",
+    "not catalogued yet": "not catalogued yet",
+    "trial": "no",
+    "forthcoming": "no",
+    "download book": "no",
+    "N/A": "no",
+    "0.5": "partially",
+    "0,5": "partially",
+    "PDF will be available": "no"
+}
 
 
 def create_institutions(df):
@@ -53,3 +73,151 @@ def create_institutions(df):
                 inst.save()
                 institutions.append(inst)
     return institutions
+
+
+def create_editions(df):
+    """ creates edition objects from specific pandas.DataFrame """
+    eds_list = []
+    ed_ct = ContentType.objects.get(app_label="editions", model="edition")
+    for i, row in df.iterrows():
+        bdate = None
+        managers = None
+        legacy_id = i+1
+        ed, _ = Edition.objects.get_or_create(
+            legacy_id=legacy_id
+        )
+        ed.name = row['Edition name']
+        ed.url = row['URL']
+        ed.scholarly = BOOLEAN_CHOICES[str(row['Scholarly'])]
+        ed.digital = BOOLEAN_CHOICES[str(row['Digital'])]
+        ed.edition = BOOLEAN_CHOICES[str(row['Edition'])]
+        if row['Language']:
+            ed.language.clear()
+            for x in row['Language'].split(';'):
+                lang, _ = Language.objects.get_or_create(
+                            iso_code=(x.strip().lower())[:3]
+                          )
+                if lang:
+                    ed.language.add(lang)
+        ed.writing_support = row['Writing support']
+
+        if row['Begin date']:
+            try:
+                bdate = pd.to_datetime(row['Begin date'])
+            except ValueError:
+                bdate = None
+            if bdate:
+                ed.begin_date = bdate
+            ed.begin_date_comment = row['Begin date']
+
+        if row['End date']:
+            try:
+                bdate = pd.to_datetime(row['End date'])
+            except ValueError:
+                bdate = None
+            if bdate:
+                ed.end_date = bdate
+            ed.end_date_comment = row['End date']
+
+        if row['Manager or Editor']:
+            managers = [
+                Person.objects.get_or_create(
+                    name=pers.strip()
+                )[0] for pers in row['Manager or Editor'].split(';')
+            ]
+            ed.manager.set(managers)
+
+        if row['Institution(s)']:
+            ed.institution.clear()
+            for x in row['Institution(s)'].split(';'):
+                try:
+                    inst = Institution.objects.get(name=x.strip())
+                except ObjectDoesNotExist:
+                    inst = None
+                if inst:
+                    ed.institution.add(inst)
+
+        ed.audience = str(row['Audience']).strip()
+        ed.philological_statement = str(row['Philological statement']).strip()
+        ed.textual_variance = str(row['Account of textual variance']).strip()
+        ed.value_witnesses = str(row['Value of witnesses']).strip()
+        ed.tei_transcription = str(row['XML-TEI Transcription']).strip()
+        ed.download = str(row['XML(-TEI) available to download']).strip()
+        ed.images = BOOLEAN_CHOICES[str(row['Images']).strip()]
+        if row['Zoom images']:
+            ed.zoom_images = BOOLEAN_CHOICES[str(row['Zoom images']).strip()]
+        if row['Image manipulation (brightness, rotation, etc.)']:
+            ed.image_manipulation = BOOLEAN_CHOICES[
+                str(row['Image manipulation (brightness, rotation, etc.)']).strip()
+            ]
+        if row['Text-Image Linking']:
+            ed.text_image = BOOLEAN_CHOICES[str(row['Text-Image Linking']).strip()]
+
+        if row['Website language']:
+            lang = None
+            ed.website_language.clear()
+            for x in row['Website language'].split(';'):
+                lang, _ = Language.objects.get_or_create(
+                            iso_code=(x.strip().lower())[:3]
+                          )
+                if lang:
+                    ed.website_language.add(lang)
+
+        if row['Glossary']:
+            ed.glossary = BOOLEAN_CHOICES[str(row['Glossary']).strip()]
+        if row['Indices']:
+            ed.indices = BOOLEAN_CHOICES[str(row['Indices']).strip()]
+        if row['String matching']:
+            ed.search = BOOLEAN_CHOICES[str(row['String matching']).strip()]
+        if row['Advanced search']:
+            ed.advanced_search = BOOLEAN_CHOICES[str(row['Advanced search']).strip()]
+        if row['Creative Commons License']:
+            ed.cc_license = row['Creative Commons License'].strip()
+        if row['Open source/Open access']:
+            ed.open_source = row['Open source/Open access'].strip()
+        if row['OCR or keyed?']:
+            ed.key_or_ocr = row['OCR or keyed?'].strip()
+        if row['Print-friendly view']:
+            ed.print_friendly = BOOLEAN_CHOICES[str(row['Print-friendly view']).strip()]
+        if row['Budget (rough)']:
+            ed.budget = row['Budget (rough)'].strip()
+        if row['Infrastructure']:
+            ed.infrastructure = row['Infrastructure'].strip()
+        if row['Current availability']:
+            ed.current_availability = True
+        else:
+            current_availability = False
+        if row['RIDE review']:
+            ed.ride_review = row['RIDE review']
+        if row['API']:
+            ed.api = BOOLEAN_CHOICES[str(row['API']).strip()]
+
+        if row['Repository of source material(s)']:
+            ed.holding_repo.clear()
+            for x in row['Repository of source material(s)'].split(';'):
+                inst, _ = Institution.objects.get_or_create(name=x.strip())
+                ed.holding_repo.add(inst)
+
+        if row['Handle-PID']:
+            pid = Pid.objects.get_or_create(
+                handle=row['Handle-PID'],
+                content_type=ed_ct,
+                object_id=ed.legacy_id
+            )
+        else:
+            pid = Pid.objects.get_or_create(
+                content_type=ed_ct,
+                object_id=ed.legacy_id
+            )
+
+        if row['Historical Period']:
+            periods = [
+                Period.objects.get_or_create(
+                    name=pers.strip()
+                )[0] for pers in row['Historical Period'].split(';')
+            ]
+            ed.historical_period.set(periods)
+
+        ed.save()
+        eds_list.append(ed)
+    return eds_list
--- editions/views.py
+++ editions/views.py
@@ -5,6 +5,7 @@ import datetime
 import requests
 import itertools
 import json
+import pandas as pd
 from django.core.validators import URLValidator
 from django.http import HttpResponse
 from django.views import generic
@@ -18,6 +19,7 @@ from .models import *
 from places import *
 from .forms import EditionForm
 from browsing.filters import EditionListFilter
+from editions.utils import BOOLEAN_CHOICES, create_institutions, create_editions
 
 
 def institution_csv(request):
@@ -74,191 +76,22 @@ def sync(request):
 def sync_status(request):
     try:
         userobject = request.user
-    except:
+    except AttributeError:
         userobject = None
     context = {}
-    context["nr_editions_start"] = len(Edition.objects.all())
+    context["nr_editions_start"] = Edition.objects.count()
+    base_url = "https://raw.githubusercontent.com/gfranzini/digEds_cat/"
 
-    url = 'https://raw.githubusercontent.com/gfranzini/digEds_cat/master/institutions_places_enriched.csv'
-    with requests.Session() as s:
-        download = s.get(url)
-        decoded_content = download.content.decode('utf-8')
-        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-        datalist = list(cr)
+    place_url = '{}master/institutions_places_enriched.csv'.format(base_url)
+    df = pd.read_csv(place_url).fillna(False)
+    create_institutions(df)
+    df = None
 
-    for x in datalist[1:]:
-        temp_inst, _ = Institution.objects.get_or_create(name=x[0])
-        temp_inst.gnd_id = x[1]
-        if x[4] != '':
-            temp_partof, _ = Place.objects.get_or_create(name=x[8])
-            temp_partof.place_type = "country"
-            temp_partof.save()
-            temp_place, _ = Place.objects.get_or_create(name=x[4])
-            temp_place.place_type = "city"
-            temp_place.part_of = temp_partof
-            try:
-                temp_place.geonames_id = x[5]
-            except:
-                pass
-            try:
-                temp_place.lat = x[6]
-            except:
-                pass
-            try:
-                temp_place.lng = x[7]
-            except:
-                pass
-            temp_place.save()
-            temp_inst.place = temp_place
-            temp_inst.save()
-        else:
-            temp_inst, _ = Institution.objects.get_or_create(name=x[0])
-        try:
-            temp_inst.lat = x[2]
-        except:
-            pass
-        try:
-            temp_inst.lng = x[3]
-        except:
-            pass
-        temp_inst.website = x[10]
-        temp_inst.gnd_id = x[1]
-        try:
-            temp_inst.save()
-        except:
-            temp_inst, _ = Institution.objects.get_or_create(name=x[0])
+    ed_url = 'https://raw.githubusercontent.com/gfranzini/digEds_cat/master/digEds_cat.csv'
+    df = pd.read_csv(ed_url).fillna(False)[:-1]
+    create_editions(df)
 
-    url = 'https://raw.githubusercontent.com/gfranzini/digEds_cat/master/digEds_cat.csv'
-    with requests.Session() as s:
-        download = s.get(url)
-        decoded_content = download.content.decode('utf-8')
-        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-        datalist = list(cr)
-        cleaned_data = []
-    for row in datalist:
-        cleaned_row = []
-        for cell in row:
-            cell = re.sub("not provided", "no information provided", cell)
-            if str(cell) == "":
-                cell = re.sub("", "not catalogued yet", cell)
-                cleaned_row.append(cell)
-            else:
-                cleaned_row.append(cell)
-        cleaned_data.append(cleaned_row)
-    BOOLEAN_CHOICES = {
-        "1": "yes",
-        "0": "no",
-        "no information provided": "no information provided",
-        "not catalogued yet": "not catalogued yet",
-        "trial": "no",
-        "forthcoming": "no",
-        "download book": "no",
-        "N/A": "no",
-        "0.5": "partially",
-        "0,5": "partially",
-        "PDF will be available": "no"}
-    counter = 1
-    Edition.objects.all().delete()
-    for row in cleaned_data[1:-1]:
-        if row[3] != "":
-            temp_per, _ = Period.objects.get_or_create(name=row[0])
-            temp_ed, _ = Edition.objects.get_or_create(legacy_id=counter)
-            temp_ed.url = row[3]
-            temp_ed.name = row[2]
-            temp_ed.scholarly = BOOLEAN_CHOICES[str(row[4])]
-            temp_ed.digital = BOOLEAN_CHOICES[str(row[5])]
-            temp_ed.edition = BOOLEAN_CHOICES[str(row[6])]
-            if row[7] != "":
-                langs1 = row[7].split(",")
-                for x in langs1:
-                    temp_lang_source, _ = Language.objects.get_or_create(
-                        iso_code=(x.strip().lower())[:3])
-                    temp_ed.language.add(temp_lang_source)
-            temp_ed.writing_support = row[8]
-            if row[9] != "":
-                start_date = re.match("\d{4}", row[9])
-                try:
-                    start_date = datetime.datetime.strptime(start_date.group(), '%Y')
-                    temp_ed.begin_date = start_date
-                except:
-                    temp_ed.begin_date_comment = str(row[9])
-            if row[10] != "":
-                end_date = re.match("\d{4}", row[10])
-                try:
-                    end_date = datetime.datetime.strptime(end_date.group(), '%Y')
-                    temp_ed.end_date = end_date
-                except:
-                    temp_ed.end_date_comment = str(row[10])
-            for pers in row[11].split(";"):
-                pers_temp, _ = Person.objects.get_or_create(name=pers.strip())
-                temp_ed.manager.add(pers_temp)
-            for inst in row[12].split(";"):
-
-                try:
-                    inst_temp = Institution.objects.get(name=inst.strip())
-                    temp_ed.institution.add(inst_temp)
-                except:
-                    pass
-            temp_ed.audience = str(row[13])
-            temp_ed.philological_statement = str(row[14])
-            temp_ed.textual_variance = str(row[15])
-            temp_ed.value_witnesses = str(row[16])
-            temp_ed.tei_transcription = str(row[17]).strip()
-            temp_ed.download = str(row[18]).strip()
-            temp_ed.images = BOOLEAN_CHOICES[str(row[19])]
-            temp_ed.zoom_images = BOOLEAN_CHOICES[str(row[20])]
-            temp_ed.image_manipulation = BOOLEAN_CHOICES[str(row[21])]
-            temp_ed.text_image = BOOLEAN_CHOICES[str(row[22])]
-            if row[24] != "":
-                langs = row[24].split(";")
-                for y in langs:
-                    temp_lang_website, _ = Language.objects.get_or_create(
-                        iso_code=(y.strip().lower())[:3])
-                    temp_ed.website_language.add(temp_lang_website)
-            temp_ed.glossary = BOOLEAN_CHOICES[str(row[25])]
-            temp_ed.indices = BOOLEAN_CHOICES[str(row[26])]
-            temp_ed.search = BOOLEAN_CHOICES[str(row[27])]
-            temp_ed.advanced_search = BOOLEAN_CHOICES[str(row[28])]
-            temp_ed.cc_license = row[29]
-            temp_ed.open_source = row[30]
-            temp_ed.key_or_ocr = str(row[37])
-            temp_ed.print_friendly = BOOLEAN_CHOICES[str(row[39])]
-            temp_ed.budget = str(row[44])
-            temp_ed.infrastructure = str(row[45])
-            temp_ed.current_availability = None
-            try:
-                alive = int(row[46])
-                if alive == 1:
-                    alive = True
-                else:
-                    alive = False
-            except:
-                alive = False
-            temp_ed.current_availability = alive
-            val = URLValidator()
-            try:
-                val(str(row[47]))
-                temp_ed.ride_review = str(row[47])
-            except:
-                temp_ed.ride_review = None
-
-            try:
-                sahlcat = int(row[48])
-                temp_ed.sahle_cat = sahlcat
-            except:
-                temp_ed.sahle_cat = None
-            # temp_ed.ride_review = str(row[47])
-            temp_ed.historical_period.add(temp_per)
-            temp_ed.api = BOOLEAN_CHOICES[str(row[32])]
-            for inst1 in row[41].split(";"):
-                try:
-                    inst_temp1, _ = Institution.objects.get_or_create(name=inst1.strip())
-                    temp_ed.holding_repo.add(inst_temp1)
-                except:
-                    pass
-            temp_ed.save()
-            counter += 1
-    context["nr_editions_now"] = len(Edition.objects.all())
+    context["nr_editions_now"] = Edition.objects.count()
     new_log = SyncLog(actor=userobject)
     new_log.save()
     context['log'] = new_log
@@ -292,7 +125,7 @@ class EditionDetailView(DetailView):
         else:
             previous_entry = edition_ids[edition_ids.index(self_id) - 1]
         context["previous_entry"] = previous_entry
-        #netvis context
+        # netvis context
         instance = Edition.objects.get(legacy_id=self_id)
         net_data = instance.netviz_data(json_out=True, show_labels=True)
         context['netviz_data'] = net_data
